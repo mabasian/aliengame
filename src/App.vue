89f6401957aa145877dc49ce5851462ff5474c11
mabasian <54101509+mabasian@users.noreply.github.com>
2023-07-09 22:13:51 +0200

    update the logic

--- src/App.vue
+++ src/App.vue
@@ -82,6 +82,16 @@
           />
         </g>
       </svg>
+      <div class="friendtalk">
+        <h3>{{ questions[questionIndex].question }}</h3>
+      </div>
+      <div class="zombietalk">
+        <p v-for="character in characterChoices" :key="character">
+          <button @click="pickQuestion(character)">
+            {{ questions[questionIndex][character] }}
+          </button>
+        </p>
+      </div>
     </section>
   </div>
 </template>
@@ -112,12 +122,22 @@ export default {
     };
   },
   computed: {
-    ...mapState(["uiState", "questions", "characterChoices", "character"]),
+    ...mapState([
+      "uiState",
+      "questions",
+      "characterChoices",
+      "character",
+      "questionIndex",
+    ]),
   },
   methods: {
     pickCharacter() {
       this.$store.commit("pickCharacter", this.characterInput);
       this.$store.commit("updateUIState", "characterChosen");
+    },
+    pickQuestion(character) {
+      this.$store.commit("pickQuestion", character);
+      // this.$store.commit("updateUIState", "questionAnswered");
     },
   },
 };
